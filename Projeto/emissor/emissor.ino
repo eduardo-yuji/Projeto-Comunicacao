93df72192b90bd5f74d1dd56880d5d38fd98cb17
Eduardo Yuji <yujiyyamada@gmail.com>
2023-11-30 21:46:59 -0300

    projeto

---
+++ Projeto/emissor/emissor.ino
@@ -0,0 +1,259 @@
+const int led = 10;
+
+String string = "";
+String binString = "";
+String aux = "";
+char modoTrans = "";
+
+void setup()
+{
+    pinMode(led, OUTPUT);
+    Serial.begin(9600);
+}
+
+/**
+ * @brief Funcao para conversão de string para binario 8 bits de acordo com a tabela ASCII
+ *
+ * @param letra
+ * @return String
+ */
+String converterTabelaAscii(char letra)
+{
+    switch (letra)
+    {
+    case 'a':
+        return "01100001";
+    case 'b':
+        return "01100010";
+    case 'c':
+        return "01100011";
+    case 'd':
+        return "01100100";
+    case 'e':
+        return "01100101";
+    case 'f':
+        return "01100110";
+    case 'g':
+        return "01100111";
+    case 'h':
+        return "01101000";
+    case 'i':
+        return "01101001";
+    case 'j':
+        return "01101010";
+    case 'k':
+        return "01101011";
+    case 'l':
+        return "01101100";
+    case 'm':
+        return "01101101";
+    case 'n':
+        return "01101110";
+    case 'o':
+        return "01101111";
+    case 'p':
+        return "01110000";
+    case 'q':
+        return "01110001";
+    case 'r':
+        return "01110010";
+    case 's':
+        return "01110011";
+    case 't':
+        return "01110100";
+    case 'u':
+        return "01110101";
+    case 'v':
+        return "01110110";
+    case 'w':
+        return "01110111";
+    case 'x':
+        return "01111000";
+    case 'y':
+        return "01111001";
+    case 'z':
+        return "01111010";
+    case ' ':
+        return "00100000";
+    default:
+        Serial.println("Fora dos parâmetros!");
+        break;
+    }
+}
+
+/**
+ * @brief Função para formar a string final passada para binario
+ *
+ * @param uni
+ * @return String
+ */
+String converterBinario(String uni)
+{
+    String stringFinal;
+    uni.toLowerCase();
+
+    for (int i = 0; i < uni.length(); i++)
+    {
+        stringFinal = stringFinal + converterTabelaAscii(uni[i]);
+    }
+    return stringFinal;
+}
+
+/**
+ * @brief Função para a transmissão dos sinais com forma de codificação NRZ-L
+ *
+ * @param uni
+ */
+void emitirNRZ_L(String uni)
+{ // uni = Palavra em binário
+    int x = 7;
+    Serial.println("Enviando NRZ-L: ");
+
+    for (int i = 0; i < uni.length(); i++)
+    {
+
+        Serial.print(uni[i]);
+
+        if (uni[i] == '0')
+        { // 0 é positivo = Led aceso
+            digitalWrite(led, HIGH);
+            delay(500);
+        }
+        else
+        { // 1 é negativo = Led apagado
+            digitalWrite(led, LOW);
+            delay(500);
+        }
+        if (i / x == 1)
+        {
+            x += 8;
+            Serial.println();
+        }
+    }
+    Serial.println();
+    digitalWrite(led, LOW);
+}
+
+/**
+ * @brief Função para a transmissão dos sinais com forma de codificação NRZ-I
+ *
+ * @param uni
+ */
+void emitirNRZ_I(String uni)
+{
+    int aux = 0;
+    int cont = 1;
+    Serial.println("Enviando NRZ-I: ");
+
+    // Inicia a transmissão com sinal baixo (LED desligado) por convenção
+    Serial.print(uni[0]);
+    digitalWrite(led, 0);
+    delay(500);
+
+    // Transmissão dos bits
+    for (int i = 1; i < uni.length(); i++)
+    {
+        cont++;
+        Serial.print(uni[i]);
+
+        if (uni[i] == '0')
+        { // 0 = mantem o sinal
+            digitalWrite(led, aux);
+            delay(500);
+        }
+        else
+        {                  // 1 = inverte o sinal
+            aux = 1 - aux; // inverte o valor de aux (entre 0 e 1)
+            digitalWrite(led, aux);
+            delay(500);
+        }
+        if(cont == 8){
+          cont = 0;
+          Serial.println();
+        }
+    }
+    Serial.println();
+    digitalWrite(led, LOW);
+}
+
+#define POLYNOMIAL 0x07
+#define WIDTH  (8 * sizeof(uint8_t))
+#define TOPBIT (1 << (WIDTH - 1))
+
+uint8_t crc8(uint8_t *data, size_t length) {
+  uint8_t remainder = 0;
+
+  for (size_t byte = 0; byte < length; ++byte) {
+    remainder ^= data[byte];
+    for (uint8_t bit = 8; bit > 0; --bit) {
+      if (remainder & TOPBIT) {
+        remainder = (remainder << 1) ^ POLYNOMIAL;
+      } else {
+        remainder = (remainder << 1);
+      }
+    }
+  }
+  return remainder;
+}
+
+void loop()
+{
+    if (Serial.available() > 0)
+    {
+        string = Serial.readString(); // Lê toda string recebida
+
+        modoTrans = string[0];
+        for (int i = 0; i < (string.length() - 1); i++)
+        {
+            string[i] = string[i + 1];
+        }
+
+        string.trim();                        // Remove espaços desnecessários na string
+        binString = converterBinario(string); // Conversão com base na tabela tabela Ascii
+
+        uint8_t binaryMessage[binString.length()/8];
+        for (int i = 0; i < binString.length(); i += 8) {
+            binaryMessage[i/8] = strtol(binString.substring(i, i+8).c_str(), NULL, 2);
+        }
+
+        uint8_t crc = crc8(binaryMessage, sizeof(binaryMessage));
+        String crcString = "";
+        crcString = crcString + String(crc, BIN);
+        for(int i=0; i<8; i++){
+          if(crcString.length()<8){
+            crcString = crcString + "0";
+          }
+        }
+        
+        Serial.println();
+        Serial.println("Mensagem para ser enviada");
+        Serial.println(string);
+        Serial.print("CRC: ");
+        Serial.println(crcString);
+        
+        binString = binString + crcString;
+
+        if (modoTrans == 'a')
+        {
+            digitalWrite(led, HIGH);
+            delay(2000);
+            emitirNRZ_L(binString);
+        }
+        else
+        {
+            if (modoTrans == 'b')
+            {
+                digitalWrite(led, HIGH);
+                delay(1000);
+                digitalWrite(led, LOW);
+                delay(1000);
+                emitirNRZ_I(binString);
+            }
+            else
+            {
+                Serial.println("Caractere para seleção inválido");
+            }
+        }
+        // Led aceso para indicar ao receptor que ele deve começar a receber informações
+    }
+}
---
+++ Projeto/receptor/receptor.ino
@@ -0,0 +1,301 @@
+String palavra = "";
+const int ldr = A0;
+int valorLdr = 0;
+int soma = 0;
+
+void setup() {
+  pinMode(ldr, INPUT);
+  Serial.begin(9600);
+}
+
+/**
+ * @brief Funcao para conversão de binario 8 bits para letra de acordo com a tabela ASCII
+ *
+ * @param uni
+ * @return char
+ */
+char converterTabelaAscii(String uni) {
+  if (uni == "01100001") {
+    return 'a';
+  } else if (uni == "01100010") {
+    return 'b';
+  } else if (uni == "01100011") {
+    return 'c';
+  } else if (uni == "01100100") {
+    return 'd';
+  } else if (uni == "01100101") {
+    return 'e';
+  } else if (uni == "01100110") {
+    return 'f';
+  } else if (uni == "01100111") {
+    return 'g';
+  } else if (uni == "01101000") {
+    return 'h';
+  } else if (uni == "01101001") {
+    return 'i';
+  } else if (uni == "01101010") {
+    return 'j';
+  } else if (uni == "01101011") {
+    return 'k';
+  } else if (uni == "01101100") {
+    return 'l';
+  } else if (uni == "01101101") {
+    return 'm';
+  } else if (uni == "01101110") {
+    return 'n';
+  } else if (uni == "01101111") {
+    return 'o';
+  } else if (uni == "01110000") {
+    return 'p';
+  } else if (uni == "01110001") {
+    return 'q';
+  } else if (uni == "01110010") {
+    return 'r';
+  } else if (uni == "01110011") {
+    return 's';
+  } else if (uni == "01110100") {
+    return 't';
+  } else if (uni == "01110101") {
+    return 'u';
+  } else if (uni == "01110110") {
+    return 'v';
+  } else if (uni == "01110111") {
+    return 'w';
+  } else if (uni == "01111000") {
+    return 'x';
+  } else if (uni == "01111001") {
+    return 'y';
+  } else if (uni == "01111010") {
+    return 'z';
+  } else if (uni == "00100000") {
+    return ' ';
+  } else {
+    Serial.println("Valor invalido");
+    return ' ';
+  }
+}
+
+#define POLYNOMIAL 0x07
+#define WIDTH (8 * sizeof(uint8_t))
+#define TOPBIT (1 << (WIDTH - 1))
+
+uint8_t crc8(uint8_t *data, size_t length) {
+  uint8_t remainder = 0;
+
+  for (size_t byte = 0; byte < length; ++byte) {
+    remainder ^= data[byte];
+    for (uint8_t bit = 8; bit > 0; --bit) {
+      if (remainder & TOPBIT) {
+        remainder = (remainder << 1) ^ POLYNOMIAL;
+      } else {
+        remainder = (remainder << 1);
+      }
+    }
+  }
+  return remainder;
+}
+
+/**
+ * @brief Função para recebimento dos sinais com forma de codificação NRZ-L
+ *
+ */
+String receptorNRZ_L() {
+  palavra = "";
+  Serial.println("");
+  Serial.println("------Leitor NRZ-L------");
+  String letraBin = "";
+  char letra = "";
+  String binString = "";
+  String receivedCRC = "";
+  int lastchar = 0;
+
+  // Enquanto não finalizar a transmissão
+  while (letraBin != "11111111" && letraBin != "00000000") {
+
+    letraBin = "";
+    letra = "";
+
+    // Lê os bits que estão chegando e os armazena em letraBin
+    for (int i = 0; i < 8; i++) {
+      valorLdr = analogRead(ldr);
+      if (valorLdr < 500) {
+        letraBin = letraBin + "1";
+      } else {
+        letraBin = letraBin + "0";
+      }
+      delay(500);
+    }
+
+    binString = binString + letraBin;
+
+    // Palavra vai se incrementando com as letras contanto que não seja o final da trasnmissão
+    if (letraBin != "11111111" && letraBin != "00000000") {
+      // letraBin é traduzida para a tabela Ascii
+      letra = converterTabelaAscii(letraBin);
+      Serial.print("Letra: ");
+      Serial.println(letra);
+      Serial.print("Binario: ");
+      Serial.println(letraBin);
+      receivedCRC = letraBin;
+      palavra = palavra + letra;
+    }
+  }
+  uint8_t binaryMessage[binString.length()/8];
+    for (int i = 0; i < binString.length(); i += 8) {
+      binaryMessage[i/8] = strtol(binString.substring(i, i+8).c_str(), NULL, 2);
+    }
+  
+  uint8_t calculatedCRC = crc8(binaryMessage, sizeof(binaryMessage)-2);
+  String crcString = "";
+  crcString = crcString + String(calculatedCRC, BIN);
+  for(int i=0; i<8; i++){
+    if(crcString.length()<8){
+      crcString = crcString + "0";
+    }
+  }
+
+  Serial.println();
+  Serial.print("CRC: ");
+  Serial.println(receivedCRC);
+  Serial.print("Calculo do CRC: ");
+  Serial.println(crcString);
+
+  if (crcString == receivedCRC) {
+    Serial.println("Dados recebidos corretamente");
+  } else {
+    Serial.println("Erro na transmissão dos dados");
+  }
+
+  return palavra;
+}
+
+/**
+ * @brief Função para recebimento dos sinais com forma de codificação NRZ-I
+ *
+ */
+String receptorNRZ_I() {
+  palavra = "";
+  Serial.println("");
+  Serial.println("------Leitor NRZ-I------");
+  String letraBin = "";
+  String palavraBin = "";
+  String sinalBin = "";
+  String receivedCRC = "";
+  String auxString = "";
+  char letra = "";
+
+  Serial.println();
+
+  // While irá ler os bits em binário recebido e concatená-los em uma
+  // string de tamanho 8, o que representa o sinal nrzi recebido de cada letra
+  while (letraBin != "01010101" && letraBin != "00000000") {
+
+    letraBin = "";
+    letra = "";    
+    
+    for (int i = 0; i < 8; i++) {
+      valorLdr = analogRead(ldr);
+      if (valorLdr > 500) {  // Led desligado
+        //Serial.print("0");
+        letraBin = letraBin + "0";
+      } else {
+        //Serial.print("1");  // Led ligado
+        letraBin = letraBin + "1";
+      }
+      delay(500);
+    }
+
+    if (letraBin != "01010101" && letraBin != "00000000") {
+      palavraBin = palavraBin + letraBin;
+    }
+
+    if (letraBin == "11111111" || letraBin == "00000000") {     
+      break;
+    }else{
+      Serial.print("Letra em Binario: ");
+      Serial.println(letraBin);
+    }
+    
+  }
+
+  // Agora que temos a palavra inteira como um sinal binário nrzi,
+  // este for irá selecionando letra por letra (cada iteração),
+  // e irá decodificá-la
+
+  for (int i = 0; i < (((palavraBin.length() - 8) / 8)); i++) {
+    sinalBin = palavraBin.substring(i * 8, i * 8 + 8);  // Seleciona letra por letra do sinal em binário, recortando espaços de tamanho 8
+    // Este for irá transformar o sinal recebido de uma letra no valor ascii binário da mesma
+    for (int j = 0; j < 8; j++) {
+      if (j == 0) {
+        letraBin = "0";
+      } else if (sinalBin[j] != sinalBin[j - 1]) {  // Decodificação mais importante do nrzi. Onde há alteração de estado, recebe 1
+        letraBin = letraBin + "1";
+      } else {
+        letraBin = letraBin + "0";  // Não há alteração de estado, recebe 0
+      }
+    }
+    if(i != ((palavraBin.length() - 8) / 8)){
+      //Serial.println(letraBin);
+      auxString = auxString + letraBin;
+      receivedCRC = letraBin;
+      letra = converterTabelaAscii(letraBin);  // Letra em binário é transformada em texto ascii
+      palavra = palavra + letra; 
+    }  
+  }
+  uint8_t binaryMessage[auxString.length()/8];
+    for (int i = 0; i < auxString.length(); i += 8) {
+      binaryMessage[i/8] = strtol(auxString.substring(i, i+8).c_str(), NULL, 2);
+    }
+  
+  uint8_t calculatedCRC = crc8(binaryMessage, sizeof(binaryMessage)-1);
+  String crcString = "";
+  crcString = crcString + String(calculatedCRC, BIN);
+  for(int i=0; i<8; i++){
+    if(crcString.length()<8){
+      crcString = crcString + "0";
+    }
+  }
+
+  receivedCRC[0]='1';
+  Serial.println();
+  Serial.print("CRC: ");
+  Serial.println(receivedCRC);
+  Serial.print("Calculo do CRC: ");
+  Serial.println(crcString);
+
+  if (crcString == receivedCRC) {
+    Serial.println("Dados recebidos corretamente");
+  } else {
+    Serial.println("Erro na transmissão dos dados");
+  }
+
+  return palavra;
+}
+
+void loop() {  // Continuará lendo (analogread) até for sinalizado com o led aceso para processar o sinal
+  valorLdr = analogRead(ldr);
+  Serial.print("Valor lido pelo LDR = ");
+  Serial.println(valorLdr);
+  delay(500);
+
+  if (valorLdr > 500) {  // Sinal alto = começar a receber
+    for (int i = 0; i < 3; i++) {
+      soma += valorLdr;
+      valorLdr = analogRead(ldr);
+      delay(500);
+    }
+
+    if (soma > 1500) {
+      palavra = receptorNRZ_L();
+      Serial.print("Palavra recebida pelo NRZ-L: ");
+      Serial.println(palavra);
+      Serial.println();
+    } else {
+      palavra = receptorNRZ_I();
+      Serial.print("Palavra recebida pelo NRZ-I: ");
+      Serial.println(palavra);
+      Serial.println();
+    }
+    soma = 0;
+  }
+}
